342df0e64889c525d2d485e083eb3946917c5063
warstyle-plato <vladislav.j.sitnikov@gmail.com>
2026-07-25 20:57:48 +0300

    Install DevelopAid hotfix through a .pth startup hook

--- developaid_hotfix/setup.py
+++ developaid_hotfix/setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name="developaid-runtime-hotfix",
     version="0.12.30",
-    py_modules=["sitecustomize"],
+    py_modules=["developaid_runtime_patch"],
+    data_files=[("", ["developaid_hotfix.pth"])],
     description="Runtime patch for DevelopAid Telegram help and Mini App close flow",
 )
