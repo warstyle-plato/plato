da2d8d9b41accda70b3a1813f223e297b4813b8a
warstyle-plato <vladislav.j.sitnikov@gmail.com>
2026-07-25 20:50:10 +0300

    Add DevelopAid runtime hotfix package

---
+++ developaid_hotfix/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="developaid-runtime-hotfix",
+    version="0.12.30",
+    py_modules=["sitecustomize"],
+    description="Runtime patch for DevelopAid Telegram help and Mini App close flow",
+)
