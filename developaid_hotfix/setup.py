5811f19730e69c4f7705d7c1f75a5702040f32e0
warstyle-plato <vladislav.j.sitnikov@gmail.com>
2026-07-25 23:43:47 +0300

    Bump Mini App close fix to 0.12.34

--- developaid_hotfix/setup.py
+++ developaid_hotfix/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="developaid-runtime-hotfix",
-    version="0.12.33",
+    version="0.12.34",
     py_modules=[
         "sitecustomize",
         "developaid_runtime_patch",
