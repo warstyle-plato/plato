8b0e31a92cb19d7d585ea6e31ce4bb8d6c518422
warstyle-plato <vladislav.j.sitnikov@gmail.com>
2026-07-25 22:15:09 +0300

    Add safe DevelopAid startup patch package

---
+++ developaid_hotfix/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="developaid-runtime-hotfix",
+    version="0.12.30.post3",
+    py_modules=["sitecustomize", "developaid_runtime_patch"],
+    description="DevelopAid Telegram help and Mini App close startup patch",
+)
