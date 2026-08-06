bd370a9246be0d47abd1cb678aa3f3068a2ba60a
warstyle-plato <vladislav.j.sitnikov@gmail.com>
2026-08-06 23:59:46 +0300

    Подключить вкладку поиска рынка

--- main_registry.py
+++ main_registry.py
@@ -3,10 +3,12 @@
 import main as _base
 from developaid_v2 import install as install_v2
 from market_search import install as install_market_search
+from market_search.ui import install as install_market_ui
 from telegram_user_registry import install
 
 app = _base.app
 core = _base.core
 registry = install(_base)
 market_search = install_market_search(app)
+install_market_ui(core)
 install_v2(app)
