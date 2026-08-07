07909e7b7bcd207ef5a5a4b6e6103ebaac942ca3
warstyle-plato <vladislav.j.sitnikov@gmail.com>
2026-08-08 01:52:37 +0300

    Install explicit MPT Telegram menu adapter

--- main_registry.py
+++ main_registry.py
@@ -2,11 +2,13 @@
 
 import main as _base
 from developaid_v2 import install as install_v2
+from mpt_bot_menu import install as install_mpt_bot_menu
 from mpt_extension import install as install_mpt
 from telegram_user_registry import install
 
 app = _base.app
 core = _base.core
 install_mpt(_base)
+install_mpt_bot_menu(_base)
 registry = install(_base)
 install_v2(app)
