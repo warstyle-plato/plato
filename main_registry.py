c2bd929f5fcfeb1562c2504b8fec6921e77fcf06
warstyle-plato <vladislav.j.sitnikov@gmail.com>
2026-08-08 01:11:25 +0300

    Install isolated MPT feature before Telegram registry wrapper

--- main_registry.py
+++ main_registry.py
@@ -2,9 +2,11 @@
 
 import main as _base
 from developaid_v2 import install as install_v2
+from mpt_extension import install as install_mpt
 from telegram_user_registry import install
 
 app = _base.app
 core = _base.core
+install_mpt(_base)
 registry = install(_base)
 install_v2(app)
