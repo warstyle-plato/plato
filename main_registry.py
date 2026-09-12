0880d07f7c6c401236b2efb0555846d063857999
warstyle-plato <vladislav.j.sitnikov@gmail.com>
2026-09-12 19:15:42 +0300

    Install TEP progress UI

--- main_registry.py
+++ main_registry.py
@@ -17,10 +17,12 @@ from mpt_extension import install as install_mpt
 from normatives_registry import install as install_normatives
 from pdf_first_page_v2 import install as install_pdf_first_page
 from telegram_user_registry import install
+from tep_progress_ui import install as install_tep_progress_ui
 
 app = _base.app
 core = _base.core
 install_pdf_first_page(core)
+install_tep_progress_ui(core)
 install_mpt(_base)
 install_mpt_bot_menu(_base)
 registry = install(_base)
